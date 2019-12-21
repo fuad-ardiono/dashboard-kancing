65105bdf9c70124b7cb0f138d02270b3c43c90ef
fuadardiono <fuadardiono16@gmail.com>
2019-12-21 11:07:27 +0800

    Update about render hello world

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <HelloWorld msg="Hello"/>
   </div>
 </template>
+
+<script>
+const HelloWorld = () => import('@/components/HelloWorld.vue');
+
+export default {
+  name: 'About',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
